5aa3be73468b9dc3290aa49ff84bb5a5093f8e83
nysh2480 <nysh2480@gmail.com>
2026-01-19 16:02:28 +0900

    Add files via upload

--- sophia_transpiler.js
+++ sophia_transpiler.js
@@ -1,8 +1,8 @@
 ﻿/*
 	File: sophia_transpiler.js
-	Role: Sophia v1.7.2 コード変換器 (Nature対応版)
+	Role: Sophia v1.7.2 コード変換器 (演算子置換強化版)
 */
-var SOPHIA = SOPHIA || {};
+var SOPHIA = window.SOPHIA || {};
 
 (function(app){
 	"use strict";
@@ -16,7 +16,8 @@ var SOPHIA = SOPHIA || {};
 
 		generate: function(ast) {
 			var buffer = [];
-			for (var i = 0; i < ast.body.length; i++) {
+			var i = 0;
+			for (i = 0; i < ast.body.length; i++) {
 				buffer.push(this.visit(ast.body[i]));
 			}
 			return buffer.join("\n");
@@ -41,45 +42,65 @@ var SOPHIA = SOPHIA || {};
 		emitScene: function(node) {
 			var code = "app.Main.register_scene('" + node.name + "', function(ctx) {\n";
 			code += "	ctx.lens = '" + node.lens + "';\n";
-			for (var i = 0; i < node.body.statements.length; i++) {
+			var i = 0;
+			for (i = 0; i < node.body.statements.length; i++) {
 				code += "	" + this.visit(node.body.statements[i]) + "\n";
 			}
 			code += "});";
 			return code;
 		},
 
+		emitAction: function(node) {
+			var nat = node.target.nature;
+			var key = node.target.ident;
+			var val = node.value;
 
-	emitAction : function(node) {
-		var nat = node.target.nature;
-		var key = node.target.ident;
-		var val = node.value;
+			// 値の中にNature参照や文字列リテラルが含まれる場合の処理
+			if (typeof val === "string") {
+				// Nature参照の置換
+				val = val.replace(/([\~#@$%\.])([a-zA-Z0-9_]+)/g, "app.NaturePool.get('$1', '$2')");
+				// Sophiaの ! (文字列結合/出力) の簡易対応
+				if (val.indexOf("!") !== -1) {
+					var parts = val.split("!");
+					val = parts[parts.length - 1].trim();
+					if (!val.startsWith('"')) val = '"' + val + '"';
+				}
+			}
 
-		// 値の中にNature参照 (~ # @ $ % .) がある場合に置換
-		if (typeof val === "string") {
-			val = val.replace(/([\~#@$%\.])([a-zA-Z0-9_]+)/g, "app.NaturePool.get('$1', '$2')");
-		}
-
-		// 物理演算 [ GeoOp ] の展開
-		if (node.op === "<") {
-			// 代入: app.NaturePool.set(nature, key, value)
-			return "app.NaturePool.set('" + nat + "', '" + key + "', " + val + ");";
-		} else if (node.op === "x") {
-			// 削除: app.NaturePool.remove(nature, key)
-			return "app.NaturePool.remove('" + nat + "', '" + key + "');";
-		} else {
-			// 算術演算 (+=, -= 等)
-			var current = "app.NaturePool.get('" + nat + "', '" + key + "')";
-			return "app.NaturePool.set('" + nat + "', '" + key + "', " + current + " " + node.op + " " + val + ");";
-		}
-	},
+			if (node.op === "<") {
+				return "app.NaturePool.set('" + nat + "', '" + key + "', " + val + ");";
+			} else if (node.op === "x") {
+				return "app.NaturePool.set('" + nat + "', '" + key + "', null);";
+			} else {
+				var current = "app.NaturePool.get('" + nat + "', '" + key + "')";
+				return "app.NaturePool.set('" + nat + "', '" + key + "', " + current + " " + node.op + " " + val + ");";
+			}
+		},
 
 		emitQA: function(node) {
 			var cond = node.condition;
-			// 自然記号 (~ # @ $ % .) をキャッチするように修正
+
+			// 1. Sophia演算子 (gt, lt, eq, ne, ge, le) を JS演算子に変換
+			var opMap = {
+				"gt": ">",
+				"lt": "<",
+				"eq": "===",
+				"ne": "!==",
+				"ge": ">=",
+				"le": "<="
+			};
+			
+			Object.keys(opMap).forEach(function(key) {
+				var reg = new RegExp("\\s" + key + "\\s", "g");
+				cond = cond.replace(reg, " " + opMap[key] + " ");
+			});
+
+			// 2. Nature参照 (~hp 等) を app.NaturePool.get に置換
 			cond = cond.replace(/([\~#@$%\.])([a-zA-Z0-9_]+)/g, "app.NaturePool.get('$1', '$2')");
 
 			var code = "if (" + cond + ") {\n";
-			for (var i = 0; i < node.body.statements.length; i++) {
+			var i = 0;
+			for (i = 0; i < node.body.statements.length; i++) {
 				code += "	" + this.visit(node.body.statements[i]) + "\n";
 			}
 			code += "}";
@@ -87,4 +108,4 @@ var SOPHIA = SOPHIA || {};
 		}
 	};
 
-})(SOPHIA);
+})(window.SOPHIA);
